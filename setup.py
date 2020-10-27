4b90d1d1b47ff88d0a89275ff515a88c2ed6ba09
yannbouteiller <yann.bouteiller@hotmail.fr>
2020-10-27 00:03:39 -0400

    pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+
+with open("description_pypi.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(name='rtgym',
       packages=['rtgym', ],
-      version='0.0.1',
+      version='0.0.1.post3',
       license='MIT',
       description='Easily implement custom OpenAI Gym environments for real-time applications',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Yann Bouteiller',
       url='https://github.com/yannbouteiller/rtgym',
       download_url='https://github.com/yannbouteiller/rtgym/archive/v0.0.1.tar.gz',
