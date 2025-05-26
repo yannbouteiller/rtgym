6ed11734ff94c463377ca82c5b5c4813ad1d3e49
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2025-05-26 10:02:31 -0400

    Release 0.16

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 
 setup(name='rtgym',
       packages=[package for package in find_packages()],
-      version='0.15',
+      version='0.16',
       license='MIT',
       description='Easily implement custom Gymnasium environments for real-time applications',
       long_description=long_description,
       long_description_content_type="text/markdown",
       author='Yann Bouteiller',
       url='https://github.com/yannbouteiller/rtgym',
-      download_url='https://github.com/yannbouteiller/rtgym/archive/refs/tags/v0.15.tar.gz',
+      download_url='https://github.com/yannbouteiller/rtgym/archive/refs/tags/v0.16.tar.gz',
       keywords=['gymnasium', 'real', 'time', 'custom', 'environment', 'reinforcement', 'learning', 'random', 'delays'],
       install_requires=['gymnasium>=1.0.0', 'numpy'],
       classifiers=[
